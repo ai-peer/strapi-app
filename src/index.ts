967aedf965d868e6e00b2f282a28cc06cc893092
sd <leowest1921@gmail.com>
2022-12-23 20:34:16 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,31 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin("graphql").service("extension");
+    extensionService.use({
+      resolversConfig: {
+        "Query.articles": {
+          /**
+           * Querying the Categories content-type
+           * bypasses the authorization system.
+           */
+          auth: false,
+        },
+        "Query.article1": {
+          /**
+           * Querying the Restaurants content-type
+           * requires the find permission
+           * on the 'Address' content-type
+           * of the 'Address' API
+           */
+          auth: {
+            scope: ["api::address.address.find"],
+          },
+        },
+      },
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
